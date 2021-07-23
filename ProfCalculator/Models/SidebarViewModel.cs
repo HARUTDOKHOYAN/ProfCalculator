bed9dee16e642201c232c1b37cc72528b4b6dae5
Davit <davit.saakian@picsart.com>
2021-07-23 15:07:27 +0400

    made slides

---
+++ ProfCalculator/Models/SidebarViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ProfCalculator.Models
+{
+    public class SidebarViewModel : INotifyPropertyChanged
+    {
+
+        private string activeMode = "Standard";
+
+        public string ActiveMode
+        {
+            get { return activeMode; }
+            set
+            {
+                activeMode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
