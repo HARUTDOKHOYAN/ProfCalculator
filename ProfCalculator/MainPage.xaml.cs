d277180abdb974c6f9c485d77e305174425165c3
Gevorg <margaryan.gevorg.25@gmail.com>
2021-07-26 10:18:22 +0400

    g

---
+++ ProfCalculator/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using ProfCalculator.Models;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ProfCalculator
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private SidebarViewModel sidebarViewModel;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            sidebarViewModel = new SidebarViewModel();
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleSidebar();
+        }
+
+        private void ToggleSidebar()
+        {
+            SidebarTransform.TranslateX = SidebarTransform.TranslateX == -400 ? 0 : -400;
+        }
+
+        private void SidebarList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item =  SidebarList.SelectedItem as TextBlock;
+            sidebarViewModel.ActiveMode = item.Text;
+
+            ToggleSidebar();
+        }
+    }
+}
---
+++ ProfCalculator/System/StandardCalc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfCalculator.System
+{
+    class StandardCalc : ICalculator
+    {
+        public string Add(string x, string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Division(string x, string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Multiply(string x, string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Percent(string x, string y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Subtraction(string x, string y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
