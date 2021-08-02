a88531b4acc155ac45130e041dfa9b38888bf768
Davit <davit.saakian@picsart.com>
2021-08-02 12:57:52 +0400

    changed namespace protection

--- ProfCalculator/Services/DataTransfer/CalcData.cs
+++ ProfCalculator/Services/DataTransfer/CalcData.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProfCalculator.Services.DataTransfer
+namespace ProfCalculator.Services
 {
     public class CalcData : INotifyPropertyChanged
     {
--- ProfCalculator/Services/DataTransfer/StandardCalcData.cs
+++ ProfCalculator/Services/DataTransfer/StandardCalcData.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProfCalculator.Services.DataTransfer
+namespace ProfCalculator.Services
 {
     class StandardCalcData: CalcData
     {
